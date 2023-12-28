3c63e22d2513d6ba0551886d3e48ac083b56437e
Jonathon E <je20k3@gmail.com>
2023-12-28 14:37:04 +0000

    Added basic Fetch functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
-console.log("JS works")
+let searchBtn = document.getElementById("search-button");
+let weatherInput = document.getElementById("search-input");
+let API_KEY = "b219bed2db60f86fada34f7841698d3e";
+let queryCity = "London"
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + queryCity  + "&appid=" + API_KEY;
 
 
+searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
 
+    if (weatherInput.value !== "") {
+        fetchForecast();
+    } else {
+        alert("Please enter a location to search for.");
+  }
+    
+});
+
+
+function fetchForecast() {
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => renderCards(data))
+        .catch(error => console.error("Error:", error));
+
+}
+
+function renderCards(data) {
+    console.log("2", data);
+}
